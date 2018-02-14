794c34bc2f581344e287655f79f135a361148528
bgonz12 <bgonz12@hotmail.com>
2018-02-13 21:21:10 -0700

    Fixed issue where TankAimingComponent.cpp header include was not at the top. grrr.

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+#include "TankAimingComponent.h"
 #include "GameFramework/Actor.h"
 #include "Components/StaticMeshComponent.h"
-#include "TankAimingComponent.h"
 
 
 // Sets default values for this component's properties
